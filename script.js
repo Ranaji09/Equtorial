00e61c30c80963c6b5490744d0e9dda0b03dedcd
Rana ji <ranabrajesh76@gmail.com>
2024-08-03 17:25:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,83 @@
+const scroll = new LocomotiveScroll({
+     el: document.querySelector('#main'),
+     smooth: true
+});
+
+
+gsap.from("a", {
+     y: 10,
+     opacity: 0,
+     duration: 1.5,
+     ease: Power2,
+     stagger: .2,
+
+})
+Shery.textAnimate("#headings h1", {
+     style: 2,
+     y: 10,
+     delay: 0.4,
+     duration: 1,
+     ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+     multiplier: 0.1,
+});
+
+gsap.from(".anim2", {
+     y: 50,
+     stagger: .3,
+     opacity: 0,
+     duration: 1,
+     ease: Expo
+})
+Shery.imageEffect("#imgntext img", {
+     style: 2, //Select Style
+     debug: true, // Debug Panel
+     config: {
+          "resolutionXY": { "value": 100 }, "distortion": { "value": true }, "mode": { "value": -9 }, "mousemove": { "value": 0 }, "modeA": { "value": 1 }, "modeN": { "value": 0 }, "speed": { "value": 1, "range": [-500, 500], "rangep": [-10, 10] }, "frequency": { "value": 50, "range": [-800, 800], "rangep": [-50, 50] }, "angle": { "value": 0.5, "range": [0, 3.141592653589793] }, "waveFactor": { "value": 1.4, "range": [-3, 3] }, "color": { "value": 10212607 }, "pixelStrength": { "value": 3, "range": [-20, 100], "rangep": [-20, 20] }, "quality": { "value": 5, "range": [0, 10] }, "contrast": { "value": 1, "range": [-25, 25] }, "brightness": { "value": 1, "range": [-1, 25] }, "colorExposer": { "value": 0.18, "range": [-5, 5] }, "strength": { "value": 0.2, "range": [-40, 40], "rangep": [-5, 5] }, "exposer": { "value": 8, "range": [-100, 100] }, "zindex": { "value": -9996999, "range": [-9999999, 9999999] }, "aspect": { "value": 0.7728029853720615 }, "ignoreShapeAspect": { "value": true }, "shapePosition": { "value": { "x": 0.010752688172043001, "y": 0 } }, "shapeScale": { "value": { "x": 0.5, "y": 0.5 } }, "shapeEdgeSoftness": { "value": 0.38, "range": [0, 0.5] }, "shapeRadius": { "value": 0, "range": [0, 2] }, "currentScroll": { "value": 0 }, "scrollLerp": { "value": 0.07 }, "gooey": { "value": false }, "infiniteGooey": { "value": false }, "growSize": { "value": 4, "range": [1, 15] }, "durationOut": { "value": 1, "range": [0.1, 5] }, "durationIn": { "value": 1.5, "range": [0.1, 5] }, "displaceAmount": { "value": 0.5 }, "masker": { "value": true }, "maskVal": { "value": 1.48, "range": [1, 5] }, "scrollType": { "value": 0 }, "geoVertex": { "range": [1, 64], "value": 1 }, "noEffectGooey": { "value": true }, "onMouse": { "value": 0 }, "noise_speed": { "value": 0.2, "range": [0, 10] }, "metaball": { "value": 0.2, "range": [0, 2] }, "discard_threshold": { "value": 0.5, "range": [0, 1] }, "antialias_threshold": { "value": 0.002, "range": [0, 0.1] }, "noise_height": { "value": 0.5, "range": [0, 2] }, "noise_scale": { "value": 10, "range": [0, 100] }
+     }
+     ,
+     preset: "./presets/wigglewobble.json",
+})
+Shery.imageEffect("#susimagewrapper img", {
+     style: 2, //Select Style
+     z: -2,
+     debug: true, // Debug Panel
+     config: {
+          "resolutionXY": { "value": 100 }, "distortion": { "value": true }, "mode": { "value": -9 }, "mousemove": { "value": 0 }, "modeA": { "value": 1 }, "modeN": { "value": 0 }, "speed": { "value": 1, "range": [-500, 500], "rangep": [-10, 10] }, "frequency": { "value": 50, "range": [-800, 800], "rangep": [-50, 50] }, "angle": { "value": 0.5, "range": [0, 3.141592653589793] }, "waveFactor": { "value": 1.4, "range": [-3, 3] }, "color": { "value": 10212607 }, "pixelStrength": { "value": 3, "range": [-20, 100], "rangep": [-20, 20] }, "quality": { "value": 5, "range": [0, 10] }, "contrast": { "value": 1, "range": [-25, 25] }, "brightness": { "value": 1, "range": [-1, 25] }, "colorExposer": { "value": 0.18, "range": [-5, 5] }, "strength": { "value": 0.2, "range": [-40, 40], "rangep": [-5, 5] }, "exposer": { "value": 8, "range": [-100, 100] }, "zindex": { "value": -9996999, "range": [-9999999, 9999999] }, "aspect": { "value": 0.7728029853720615 }, "ignoreShapeAspect": { "value": true }, "shapePosition": { "value": { "x": 0.010752688172043001, "y": 0 } }, "shapeScale": { "value": { "x": 0.5, "y": 0.5 } }, "shapeEdgeSoftness": { "value": 0.38, "range": [0, 0.5] }, "shapeRadius": { "value": 0, "range": [0, 2] }, "currentScroll": { "value": 0 }, "scrollLerp": { "value": 0.07 }, "gooey": { "value": false }, "infiniteGooey": { "value": false }, "growSize": { "value": 4, "range": [1, 15] }, "durationOut": { "value": 1, "range": [0.1, 5] }, "durationIn": { "value": 1.5, "range": [0.1, 5] }, "displaceAmount": { "value": 0.5 }, "masker": { "value": true }, "maskVal": { "value": 1.48, "range": [1, 5] }, "scrollType": { "value": 0 }, "geoVertex": { "range": [1, 64], "value": 1 }, "noEffectGooey": { "value": true }, "onMouse": { "value": 0 }, "noise_speed": { "value": 0.2, "range": [0, 10] }, "metaball": { "value": 0.2, "range": [0, 2] }, "discard_threshold": { "value": 0.5, "range": [0, 1] }, "antialias_threshold": { "value": 0.002, "range": [0, 0.1] }, "noise_height": { "value": 0.5, "range": [0, 2] }, "noise_scale": { "value": 10, "range": [0, 100] }
+     },
+     preset: "./presets/wigglewobble.json",
+})
+gsap.from("#imgntext img", {
+     z: -2,
+     duration: 2
+})
+Shery.imageEffect("#picimg img", {
+     style: 2, //Select Style
+     z: -2,
+     debug: true, // Debug Panel
+     config: { "resolutionXY": { "value": 100 }, "distortion": { "value": true }, "mode": { "value": -9 }, "mousemove": { "value": 1 }, "modeA": { "value": 0 }, "modeN": { "value": 1 }, "speed": { "value": 1, "range": [-500, 500], "rangep": [-10, 10] }, "frequency": { "value": 50, "range": [-800, 800], "rangep": [-50, 50] }, "angle": { "value": 0.5, "range": [0, 3.141592653589793] }, "waveFactor": { "value": 1.4, "range": [-3, 3] }, "color": { "value": 10212607 }, "pixelStrength": { "value": 3, "range": [-20, 100], "rangep": [-20, 20] }, "quality": { "value": 5, "range": [0, 10] }, "contrast": { "value": 1, "range": [-25, 25] }, "brightness": { "value": 1, "range": [-1, 25] }, "colorExposer": { "value": 0.18, "range": [-5, 5] }, "strength": { "value": 0.2, "range": [-40, 40], "rangep": [-5, 5] }, "exposer": { "value": 8, "range": [-100, 100] }, "zindex": { "value": -9996999, "range": [-9999999, 9999999] }, "aspect": { "value": 0.7728029853720615 }, "ignoreShapeAspect": { "value": true }, "shapePosition": { "value": { "x": 0.010752688172043001, "y": 0 } }, "shapeScale": { "value": { "x": 0.5, "y": 0.5 } }, "shapeEdgeSoftness": { "value": 0.38, "range": [0, 0.5] }, "shapeRadius": { "value": 0, "range": [0, 2] }, "currentScroll": { "value": 0 }, "scrollLerp": { "value": 0.07 }, "gooey": { "value": false }, "infiniteGooey": { "value": false }, "growSize": { "value": 4, "range": [1, 15] }, "durationOut": { "value": 1, "range": [0.1, 5] }, "durationIn": { "value": 1.5, "range": [0.1, 5] }, "displaceAmount": { "value": 0.5 }, "masker": { "value": true }, "maskVal": { "value": 1.48, "range": [1, 5] }, "scrollType": { "value": 0 }, "geoVertex": { "range": [1, 64], "value": 1 }, "noEffectGooey": { "value": true }, "onMouse": { "value": 0 }, "noise_speed": { "value": 0.2, "range": [0, 10] }, "metaball": { "value": 0.2, "range": [0, 2] }, "discard_threshold": { "value": 0.5, "range": [0, 1] }, "antialias_threshold": { "value": 0.002, "range": [0, 0.1] }, "noise_height": { "value": 0.5, "range": [0, 2] }, "noise_scale": { "value": 10, "range": [0, 100] } },
+
+     preset: "./presets/wigglewobble.json",
+})
+gsap.from("#picing img", {
+     z: -2,
+     duration: 2
+})
+
+Shery.imageEffect("#bimg img", {
+     style: 2,
+     gooey: true,
+})
+document.querySelector("#ftext button").addEventListener("mouseover", function () {
+     gsap.to("#future video", {
+          opacity: 1,
+          duration: 1,
+          ease: Power2,
+     })
+})
+document.querySelector("#ftext button").addEventListener("mouseleave", function () {
+     gsap.to("#future video", {
+          opacity: 0,
+          duration: 1,
+          ease: Power2,
+     })
+})
